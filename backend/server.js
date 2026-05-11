e575db1e491dd58c7f0ae257e480995986b2eb0c
Danial2k00 <you@example.com>
2026-05-11 15:36:47 +0530

    Fixed production CORS

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,10 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "*",
+    origin: [
+      "http://localhost:5173",
+      "https://trading-learning-website.vercel.app"
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
